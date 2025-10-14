dcedaadeb567a362302567c3830d258f0b62db88
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-10-14 17:23:31 +0530

    Implement Checkout Page with styling and update routing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { HomePage } from "./pages/HomePage";
 import { Route, Routes } from "react-router";
+import { CheckOutPage } from "./pages/CheckoutPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<div>Test</div>} />
+      <Route path="/checkout" element={<CheckOutPage />} />
     </Routes>
   );
 }
