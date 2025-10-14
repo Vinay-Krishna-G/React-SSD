0dd353496821c82cd4443810d202fb5a1011734c
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-10-14 17:41:10 +0530

    Add Orders page with routing and styling; remove legacy HTML file

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import { HomePage } from "./pages/HomePage";
 import { Route, Routes } from "react-router";
 import { CheckOutPage } from "./pages/CheckoutPage";
 import "./App.css";
+import { Orders } from "./pages/Orders";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/orders" element={<Orders />} />
     </Routes>
   );
 }
