2a1187cd279b156131db82f0322e6053a2b1b4b1
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-10-15 13:04:35 +0530

    Add Tracking page with styling and favicon integration

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import { CheckOutPage } from "./pages/checkout/CheckoutPage";
 import "./App.css";
 import { Orders } from "./pages/Orders";
+import { Tracking } from "./pages/Tracking";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckOutPage />} />
       <Route path="/orders" element={<Orders />} />
+      <Route path="/tracking" element={<Tracking />} />
     </Routes>
   );
 }
